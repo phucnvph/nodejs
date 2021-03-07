cbca7818b5c582c1728a18c442ea6c2684bba288
Phuc Van Nguyen <nguyenvanphucbk8@gmail.com>
2021-03-08 00:02:37 +0700

    leaning Routes middleware

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const path = require('path');
 
-app.get('/', (req, res) => {
-  res.send('Hello Worlddddsdfdfd!')
+const app = express();
+const port = 3000;
+
+app.use('/public', express.static(path.join(__dirname, '/public')));
+app.get('/user/:id', function (req, res, next) {
+  console.log('Request URL:', req.originalUrl);
+  console.log('ID:', req.params.id);
+  if(req.params.id <= 0) {
+    res.send('Dừng');
+  } else{
+    next();
+  }
+}, function (req, res, next) {
+  console.log('Request Type:', req.method);
+  res.send('User Info');
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`Running Server: http://localhost:${port}`);
 })
